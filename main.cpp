c3770ca48b5e38b33c8f484d866941e3b2064fba
Matteo <matteo@pop-os.localdomain>
2021-10-22 12:04:29 +0200

    Added graph creation and data

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "lib/utimer.hpp"
+#include "lib/queue.hpp"
+#include "lib/graph.hpp"
+#include <vector>
+#include <utility>
+
+
+int main(int argc, char* argv[]) {
+
+    // Graph graph = Graph();
+
+    // graph.createGraph();
+
+    // std::vector<std::pair<uint,uint>> edges = graph.getEdges();
+
+    // for (std::pair<uint,uint> pair : edges) {
+    //     std::cout << "Elements: " << pair.first << "--" << pair.second << std::endl;
+    // }
+
+    // return (0);
+
+}
